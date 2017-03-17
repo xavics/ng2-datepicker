2b4158cd8da5f72411b889075b72d62846a1f018
xavics <xacosa@gmail.com>
2017-03-17 10:37:15 +0100

    check

---
+++ lib-dist/ng2-datepicker.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { SlimScrollModule } from 'ng2-slimscroll';
+
+import { DatePickerComponent } from './ng2-datepicker.component';
+import {Ng2DeviceDetector} from "ng2-device-detector";
+export { DatePickerOptions, DateModel } from './ng2-datepicker.component';
+
+@NgModule({
+  declarations: [
+    DatePickerComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SlimScrollModule,
+    Ng2DeviceDetector
+  ],
+  exports: [
+    DatePickerComponent,
+    SlimScrollModule,
+    FormsModule,
+    Ng2DeviceDetector
+  ]
+})
+export class DatePickerModule { }
